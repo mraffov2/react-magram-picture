087b3e0388192fd6ce4c206c7cba0fa4929baee7
Mario Villegas <mraffov@gmail.com>
2019-09-06 18:43:50 -0400

    Updates

---
+++ precache-manifest.d404377c70655c41e38cfe290deb8258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7740345a36787316e94fda7f19ead5ed",
+    "url": "/react-magram-picture/index.html"
+  },
+  {
+    "revision": "3071755c855e45717c30",
+    "url": "/react-magram-picture/static/js/2.1478327a.chunk.js"
+  },
+  {
+    "revision": "5dce40f37cafd2e87fa8",
+    "url": "/react-magram-picture/static/js/main.5b13235e.chunk.js"
+  },
+  {
+    "revision": "db0380d6472ba33bda13",
+    "url": "/react-magram-picture/static/js/runtime~main.c7530dc2.js"
+  },
+  {
+    "revision": "f98b32ceb4e65ec11ae685fc4b6d5a15",
+    "url": "/react-magram-picture/static/media/no-image-icon.f98b32ce.png"
+  },
+  {
+    "revision": "b7bb8b52986904db568ade87e565fdae",
+    "url": "/react-magram-picture/static/media/phone.b7bb8b52.png"
+  }
+]);
